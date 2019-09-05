9fdc3e5d6c11f5af4a738571434a024f7ba05847
manhhai <39932592+manhhai98@users.noreply.github.com>
2019-09-05 22:56:59 +0700

    add frontend

--- php/add.php
+++ php/add.php
@@ -1,9 +1,4 @@
-<html>
-<head>
-	<title>Add Data</title>
-</head>
 
-<body>
 <?php
 //including the database connection file
 include_once("config.php");
@@ -42,5 +37,52 @@ if(isset($_POST['Submit'])) {
 	}
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add Data</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="style/add.css">
+	<link rel="stylesheet" href="style/manager/header.css">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+	<?php include 'include/manager/header.html';?>
+
+	<div class="container">
+		<div class="head">
+			<a href="index.php" class="text1">Home</a>
+			>
+			<a href="add.php" class="text2">Add new data</a>	
+		</div>
+
+		<div class="main">
+			<h2 style="text-align: center;">Add new data</h2>
+			<form action="add.php" method="post" name="form1" class="form">
+			  <div class="form-group">
+			    <label for="name">Name:</label>
+			    <input type="text" class="form-control" name="name" placeholder="Enter name">
+			  </div>
+			  <div class="form-group">
+			    <label for="age">Age:</label>
+			    <input type="text" class="form-control" name="age" placeholder="Enter age">
+			  </div>
+			  <div class="form-group">
+			    <label for="email">Email:</label>
+			    <input type="text" class="form-control" name="email" placeholder="Enter email">
+			  </div>
+			   	<button type="reset" class="btn btn-danger float-right" name="cancel">Cancel</button>
+			  	<button type="submit" class="btn btn-primary float-right" name="Submit">Add</button>
+
+			  <div class="clearfix"></div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
+
--- php/index.php
+++ php/index.php
@@ -1,3 +1,4 @@
+
 <?php
 //including the database connection file
 include_once("config.php");
@@ -6,33 +7,66 @@ include_once("config.php");
 //$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
 ?>
-
-<html>
-<head>	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
 	<title>Homepage</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="style/index.css">
+	<link rel="stylesheet" href="style//manager/header.css">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
+	<?php include 'include/manager/header.html';?>
 
-	<table width='80%' border=0>
+	<div class="container" style="margin-top: 50px;">
+			<div class="float-left">
+				<button type="button" class="btn btn-primary"><a href="add.php">Add New Data</a></button>
+			</div>
 
-	<tr bgcolor='#CCCCCC'>
-		<td>Name</td>
-		<td>Age</td>
-		<td>Email</td>
-		<td>Update</td>
-	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
-		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-	}
-	?>
-	</table>
+			<div class="float-right">
+				<form action="" method="GET"> 
+				  <div class="row">
+				      <div class="input-group">
+				        <input type="text" class="form-control" placeholder="Search" id="txtSearch"/>
+				        <div class="input-group-btn">
+				          <button class="btn btn-primary" type="button">
+				            <span class="glyphicon glyphicon-search"></span> Search
+				          </button>
+				        </div>
+				      </div>
+				  </div>
+				</form>
+			</div>
+
+			<div class="clearfix"></div>
+			<table class="table">
+			  <thead class="thead-dark">
+			    <tr>
+			      <th scope="col">Name</th>
+			      <th scope="col">Age</th>
+			      <th scope="col">Email</th>
+			      <th scope="col">Update</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    	<?php 
+						//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+						while($res = mysqli_fetch_array($result)) { 		
+							echo "<tr>";
+							echo "<td>".$res['name']."</td>";
+							echo "<td>".$res['age']."</td>";
+							echo "<td>".$res['email']."</td>";	
+							echo "<td><button type="button" class="btn btn-primary edit"><a href=\"edit.php?id=$res[id]\">Edit</a></button> | <button type="button" class="btn btn-danger delete"><a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></button</td>";		
+						}
+						?>
+			  </tbody>
+			</table>
+	</div>
 </body>
 </html>
