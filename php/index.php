9c733e318b246338f1b1fdc7467f2d23eb3be258
manhhai <39932592+manhhai98@users.noreply.github.com>
2019-09-06 00:04:57 +0700

    fix error

--- php/index.php
+++ php/index.php
@@ -62,7 +62,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 							echo "<td>".$res['name']."</td>";
 							echo "<td>".$res['age']."</td>";
 							echo "<td>".$res['email']."</td>";	
-							echo "<td><button type="button" class="btn btn-primary edit"><a href=\"edit.php?id=$res[id]\">Edit</a></button> | <button type="button" class="btn btn-danger delete"><a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></button</td>";		
+							echo "<td><button type=\"button\" class=\"btn btn-primary edit\"><a href=\"edit.php?id=$res[id]\">Edit</a></button> | <button type=\"button\" class=\"btn btn-danger delete\"><a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></button></td>";		
 						}
 						?>
 			  </tbody>
