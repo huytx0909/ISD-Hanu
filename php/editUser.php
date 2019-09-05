6a9131b2fb46dc16db45a61828a5806e40121b65
manhhai <39932592+manhhai98@users.noreply.github.com>
2019-09-06 00:30:56 +0700

    add frontend

--- php/editUser.php
+++ php/editUser.php
@@ -48,34 +48,55 @@ while($res = mysqli_fetch_array($result))
 	$email = $res['email'];
 }
 ?>
-<html>
-<head>	
-	<title>Edit Data</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Edit</title>
 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="style/edit.css">
+	<link rel="stylesheet" href="style//manager/header.css">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
 <body>
-	<a href="index.php">Home</a>
-	<br/><br/>
-	
-	<form name="form1" method="post" action="edit.php">
-		<table border="0">
-			<tr> 
-				<td>Name</td>
-				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
-			</tr>
-			<tr> 
-				<td>Age</td>
-				<td><input type="text" name="age" value="<?php echo $age;?>"></td>
-			</tr>
-			<tr> 
-				<td>Email</td>
-				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
-			</tr>
-		</table>
-	</form>
+	<?php include 'include/manager/header.html';?>
+
+	<div class="container">
+		<div class="head">
+			<a href="index.php" class="text1">Home</a>
+			>
+			<a href="add.html" class="text2">Edit</a>	
+		</div>
+
+		<div class="main">
+			<h2 style="text-align: center;">Edit</h2>
+			<form action="add.php" method="post" name="form1" class="form">
+			  <div class="form-group">
+			    <label for="name">Name:</label>
+			    <input type="text" class="form-control" name="name" value="<?php echo $name;?>">
+			  </div>
+			  <div class="form-group">
+			    <label for="age">Age:</label>
+			    <input type="text" class="form-control" name="age" value="<?php echo $age;?>">
+			  </div>
+			  <div class="form-group">
+			    <label for="email">Email:</label>
+			    <input type="text" class="form-control" name="email" value="<?php echo $email;?>">
+			  </div>
+			  <div class="form-group">
+			    <input type="hidden" name="id" value=<?php echo $_GET['id'];?>>
+			  </div>
+			  	
+			   	<button type="reset" class="btn btn-danger float-right" name="cancel">Cancel</button>
+			  	<button type="submit" class="btn btn-primary float-right" name="update">Update</button>
+
+			  <div class="clearfix"></div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
