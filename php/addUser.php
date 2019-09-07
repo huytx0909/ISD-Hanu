607c080b4e4c082319e0b592e0f29d55e4b9cee5
manhhai <39932592+manhhai98@users.noreply.github.com>
2019-09-07 12:38:50 +0700

    fix frontend

--- php/addUser.php
+++ php/addUser.php
@@ -2,6 +2,8 @@
 	//including the database connection file
 	include_once("config.php");
 
+	$nameErr=$passwordErr=$emailErr=$phoneErr=$addressErr=$salaryErr=$departmentErr=$teamErr= $roleErr =$success="";
+
 	if(isset($_POST['Submit'])) {	
 		$userName = mysqli_real_escape_string($mysqli, $_POST['user-name']);
 		$password = mysqli_real_escape_string($mysqli, $_POST['password']);
@@ -14,36 +16,59 @@
 		$role = mysqli_real_escape_string($mysqli, $_POST['role']);
 			
 		// checking empty fields
-		if(empty($userName) || empty($password) || empty($email)) {
+		if(empty($userName) || empty($password) || empty($email) || empty($phone) || empty($address) || empty($salary) || empty($department) || empty($team) || empty($role)) {
 					
 			if(empty($userName)) {
-				echo "<font color='red'>Name field is empty.</font><br/>";
+				$nameErr = "Name field is empty";
 			}
 			
 			if(empty($password)) {
-				echo "<font color='red'>password field is empty.</font><br/>";
+				$passwordErr = "Password field is empty";
 			}
 			
 			if(empty($email)) {
-				echo "<font color='red'>Email field is empty.</font><br/>";
+				$emailErr = "Email field is empty";
+			}
+
+			if(empty($phone)) {
+				$phoneErr = "Phone field is empty";
 			}
 			
-			//link to the previous ppassword
-			echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+			if(empty($address)) {
+				$addressErr = "Address field is empty";
+			}
+			
+			if(empty($salary)) {
+				$salaryErr = "Salary field is empty";
+			}
+
+			if(empty($department)) {
+				$departmentErr = "Department field is empty";
+			}
+			
+			if(empty($team)) {
+				$teamErr = "Team field is empty";
+			}
+			
+			if(empty($role)) {
+				$roleErr = "Role field is empty";
+			}
+			
 		} else { 
 			// if all the fields are filled (not empty) 
 
 			//insert data to database	
 			echo $userName, $password, $email, $phone, $address, $salary, $department, $team, $role;
-			$insertResult = mysqli_query($mysqli, "INSERT INTO user(`name`, password, email, phone, address, salary) VALUES('$userName','$password','$email')");
+			$insertResult = mysqli_query($mysqli, "INSERT INTO user(`name`, password, email, phone, address, salary, department, team, role) VALUES('$userName','$password','$email','$phone','$address','$salary','$department','$team','$role')");
 			
 			//display success message
-			echo "<font color='green'>Data added successfully.";
-			echo "<br/><a href='index.php'>View Result</a>";
+			$success = "Data added successfully.";
 		}
 	}
 
 			$roleResult = mysqli_query($mysqli, "SELECT * FROM role ORDER BY id DESC"); // using mysqli_query instead
+			$departmentResult = mysqli_query($mysqli, "SELECT * FROM department ORDER BY id DESC"); // using mysqli_query instead
+			$teamResult = mysqli_query($mysqli, "SELECT * FROM team ORDER BY id DESC"); // using mysqli_query instead
 	?>
 	<!DOCTYPE html>
 	<html lang="en">
@@ -53,62 +78,105 @@
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" href="style/add.css">
-		<link rel="stylesheet" href="style/manager/header.css">
+		<link rel="stylesheet" href="style/header.css">
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<?php include 'include/manager/header.html';?>
+		<?php include 'include/header.html';?>
 
 		<div class="container">
-			<div class="head">
-				<a href="index.php" class="text1">Home</a>
-				>
-				<a href="addUser.php" class="text2">Add new data</a>	
-			</div>
 
 			<div class="main">
 				<h2 style="text-align: center;">Add new data</h2>
 				<form action="addUser.php" method="post" name="form1" class="form">
+					<span class="success"><?php echo $success; ?></span>
 				  <div class="form-group">
-				    <label for="name">User Name:</label>
+				    <label for="name">User Name:</label><br>
+				    <span class="error"><?php echo $nameErr; ?></span>
 				    <input type="text" class="form-control" name="user-name" placeholder="Enter user name">
 				  </div>
+
 				  <div class="form-group">
-				    <label for="password">Password:</label>
+				    <label for="password">Password:</label><br>
+				    <span class="error"><?php echo $passwordErr; ?></span>
 				    <input type="password" class="form-control" name="password" placeholder="Enter password">
 				  </div>
+
 				  <div class="form-group">
-				    <label for="email">Email:</label>
+				    <label for="email">Email:</label><br>
+				    <span class="error"><?php echo $emailErr; ?></span>
 				    <input type="text" class="form-control" name="email" placeholder="Enter email">
 				  </div>
+
 				   <div class="form-group">
-				    <label for="phone">Phone:</label>
+				    <label for="phone">Phone:</label><br>
+				    <span class="error"><?php echo $phoneErr; ?></span>
 				    <input type="text" class="form-control" name="phone" placeholder="Enter phone">
 				  </div>
+
 				  <div class="form-group">
-				    <label for="address">Address:</label>
+				    <label for="address">Address:</label><br>
+				    <span class="error"><?php echo $addressErr; ?></span>
 				    <input type="text" class="form-control" name="address" placeholder="Enter address">
 				  </div>
+				
 				  <div class="form-group">
-				    <label for="salary">Salary:</label>
+				    <label for="salary">Salary:</label><br>
+				    <span class="error"><?php echo $salaryErr; ?></span>
 				    <input type="text" class="form-control" name="salary" placeholder="Enter salary">
 				  </div>
-				  <div class="form-group">
-				    <label for="department">Department:</label>
-				    <input type="text" class="form-control" name="department" placeholder="Enter department">
+				<div class="select-group">
+				  <div class="form-group select">
+				  		<label for="department">Department:</label><br>
+				  		<span class="error"><?php echo $departmentErr; ?></span>
+				  		<br>
+				  		
+				    	<select class="custom-select" name="department">
+				    		<option></option>
+					   		<?php
+					   		while($res = mysqli_fetch_array($departmentResult)) { ?> 
+					      	<option  value=<?php echo "<td>".$res['name']."</td>"?>>
+
+					      	 <?php echo "<td>".$res['name']."</td>"?>    		
+					      	</option>									
+							<?php
+						 }
+						?>
+						</select>
 				  </div>
-				  <div class="form-group">	
-				    <label for="team">Team:</label>
-				    <input type="text" class="form-control" name="team" placeholder="Enter team">
+
+				  <div class="form-group select">
+				  		<label for="team">Team:</label><br>
+				  		<span class="error"><?php echo $teamErr; ?></span>
+				  		<br>
+				  		
+				    	<select class="custom-select" name="team">
+				    		<option></option>
+					   		<?php
+					   		while($res = mysqli_fetch_array($teamResult)) { ?> 
+					      	<option value=<?php "<td>".$res['name']."</td>"?>>
+
+					      	 <?php echo "<td>".$res['name']."</td>"?>    		
+					      	</option>									
+							<?php
+						 }
+						?>
+						</select>
 				  </div>
-				    <div class="form-group">
+
+				    <div class="form-group select">
+				    	<label for="role">Role:</label><br>
+				    	<span class="error"><?php echo $roleErr; ?></span>
+				    	<br>
+				    	
 					   <select class="custom-select" name="role">
+					   		<option></option>
 					   		<?php
 					   		while($res = mysqli_fetch_array($roleResult)) { ?> 
-					      	<option class="dropdown-item" value=<?php echo "<td>".$res['name']."</td>"?>>
+					      	<option value=<?php echo "<td>".$res['name']."</td>"?>>
 
 					      	 <?php echo "<td>".$res['name']."</td>"?>    		
 					      	</option>									
@@ -117,12 +185,10 @@
 						?>
 						</select>
 						
-					
-					  </div>
 					</div>
-				  </div>
+				</div>
 
-				   	<button type="reset" class="btn btn-danger float-right" name="cancel">Cancel</button>
+				   	<button type="reset" class="btn btn-danger float-right" name="cancel" >Cancel</button>
 				  	<button type="submit" class="btn btn-primary float-right" name="Submit">Add</button>
 
 				  <div class="clearfix"></div>
--- php/index.php
+++ php/index.php
@@ -15,14 +15,14 @@ $result = mysqli_query($mysqli, "SELECT * FROM user ORDER BY id DESC"); // using
 	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="style/index.css">
-	<link rel="stylesheet" href="style//manager/header.css">
+	<link rel="stylesheet" href="style/header.css">
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
-	<?php include 'include/manager/header.html';?>
+	<?php include 'include/header.html';?>
 
 	<div class="container" style="margin-top: 50px;">
 			<div class="float-left">
@@ -48,10 +48,17 @@ $result = mysqli_query($mysqli, "SELECT * FROM user ORDER BY id DESC"); // using
 			<table class="table">
 			  <thead class="thead-dark">
 			    <tr>
-			      <th scope="col">Name</th>
-			      <th scope="col">Age</th>
+			      <th scope="col">Username</th>
+			      <th scope="col">Password</th>
 			      <th scope="col">Email</th>
-			      <th scope="col">Update</th>
+			      <th scope="col">Phone</th>
+			      <th scope="col">Address</th>
+			      <th scope="col">Salary</th>
+			      <th scope="col">Department</th>
+			      <th scope="col">Team</th>
+			      <th scope="col">Role</th>
+			      <th scope="col">Date Created</th>
+			      <th scope="col">Actions</th>
 			    </tr>
 			  </thead>
 			  <tbody>
@@ -59,9 +66,15 @@ $result = mysqli_query($mysqli, "SELECT * FROM user ORDER BY id DESC"); // using
 						//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
 						while($res = mysqli_fetch_array($result)) { 		
 							echo "<tr>";
-							echo "<td>".$res['name']."</td>";
-							echo "<td>".$res['age']."</td>";
-							echo "<td>".$res['email']."</td>";	
+							echo "<td class=\"cell-breakWord\">".$res['username']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['password']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['email']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['phone']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['address']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['salary']."</td>";	
+							echo "<td class=\"cell-breakWord\">".$res['department']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['team']."</td>";
+							echo "<td class=\"cell-breakWord\">".$res['role']."</td>";		
 							echo "<td><button type=\"button\" class=\"btn btn-primary edit\"><a href=\"editUser.php?id=$res[id]\">Edit</a></button> | <button type=\"button\" class=\"btn btn-danger delete\"><a href=\"deleteUser.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></button></td>";		
 						}
 						?>
